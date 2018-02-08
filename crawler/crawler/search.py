dc8734d5feb6eb826fb99c1429826af11978341d
diegofsousa <diegofernando5672@gmail.com>
2018-02-08 05:39:56 -0300

    more one

--- crawler/crawler/search.py
+++ crawler/crawler/search.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 from utils import (to_compose_search_string,
 				   duck_search_string,
 				   select_lyrics_link,
-				   work_in_request_lyrics_url,
+				   work_in_request_url,
 				   select_lyric_in_html,
 				   select_number_views,
 				   select_gender,
-				   select_music_cipher_url)
+				   select_music_cipher_url,
+				   select_music_chords)
 
 class SearchLyricsData(object):
 	"""docstring for Search"""
@@ -16,24 +17,31 @@ class SearchLyricsData(object):
 		self.__artist = artist
 		self.__music = music
 		self.__search_string = to_compose_search_string(artist, music)
-		self.__driver = webdriver.PhantomJS()
+		self.__driver = webdriver.Firefox()
 		self.__lyrics_url = self.__ask_duck_for_lyrics_url__()
 		self.__lyrics_html = self.__request_lyrics_url__()
+		self.__driver.quit()
 
 	def __repr__(self):
 		return "<(SearchLyricsDataObject) artist: {}, music: {}, lyrics url: {}>".format(
 						self.__artist, self.__music, self.__lyrics_url)
 
 	def __ask_duck_for_lyrics_url__(self):
+		print("Request duck go search url...", end='', flush=True)
 		try:
 			self.__driver.get(duck_search_string(self.__search_string))
+			print("  Ok!")
+			#self.__driver.quit()
 			return select_lyrics_link(self.__driver.page_source)
 		except Exception as e:
-			print(e)
+			print(" "+ e)
 			return None
 
 	def __request_lyrics_url__(self):
-		return work_in_request_lyrics_url(self.__lyrics_url)
+		if self.__lyrics_url:
+			print("Request lyrics url...", end='', flush=True)
+			return work_in_request_url(self.__lyrics_url)
+		return None
 
 	def total_return_for_lyrics(self):
 		return (self.__artist,
@@ -65,12 +73,25 @@ class SearchLyricsData(object):
 		return None
 		
 
-anita = SearchLyricsData("anitta", "vai malandra")
-print(anita)
-print(anita.total_return_for_lyrics())
+class SearchCipherData(object):
+	"""docstring for SearchCipherData"""
+	def __init__(self, cipher_url):
+		super(SearchCipherData, self).__init__()
+		self.__cipher_url = cipher_url
+		self.__cipher_html = self.__request_cipher_url__()
 
+	def __request_cipher_url__(self):
+		print("Request cipher url...", end='', flush=True)
+		return work_in_request_url(self.__cipher_url)
 
+	def number_of_chords(self):
+		if self.__cipher_html:
+			return len(select_music_chords(self.__cipher_html))
+		return None
 
+anita_l = SearchLyricsData("tom jobim", "desafinado")
+if anita_l.total_return_for_lyrics()[6]:
+	anita_c = SearchCipherData(anita_l.total_return_for_lyrics()[6])
 
-
-		
+print(anita_l)
+print(anita_c.number_of_chords())
--- crawler/crawler/utils.py
+++ crawler/crawler/utils.py
@@ -1,6 +1,7 @@
 from scrapy.selector import Selector as scp
 import requests as req
 from requests.exceptions import RequestException
+import json
 
 def to_compose_search_string(artist, music):
 	search_string = "'{} {}' site:www.letras.mus.br".format(artist, music)
@@ -10,35 +11,39 @@ def duck_search_string(search_string):
 	return "https://duckduckgo.com/?q=" + search_string
 
 def select_lyrics_link(page_source):
-	lyrics_link = scp(text=page_source).xpath('//a[@class="result__a"]/@href').extract_first()
-	return lyrics_link.replace("traducao.html","")
+	try:
+		lyrics_link = scp(text=page_source).xpath('//a[@class="result__a"]/@href').extract_first()
+		return lyrics_link.replace("traducao.html","")
+	except Exception as e:
+		return None
 
-def work_in_request_lyrics_url(url):
+def work_in_request_url(url):
 	try:
 		request = req.get(url)
 		if request.status_code == 200:
+			print("  Ok!")
 			return request.content
-		print("Not status 200")
+		print(" Not status 200, trying again...")
 		return None
 	except RequestException as e:
-		print(e)
+		print(" "+e)
 		try:
 			request = req.get(url)
 			if request.status_code == 200:
+				print("  Ok!")
 				return request.content
-			print("Not status 200")
+			print(" Not status 200")
 			return None
 		except RequestException as e:
-			print(e)
+			print(" "+e)
 
 def select_lyric_in_html(page_source):
 	lyric = scp(text=page_source).xpath('//div[@class="cnt-letra p402_premium"]/article/p/text()').extract()
 	return list(set(' '.join(lyric).lower().split(' ')))
 
 def select_number_views(page_source):
-	number_views = list(scp(text=page_source).xpath('//div[@class="cnt-info_exib"]/b/text()').extract_first())
-	number_views.remove(".")
-	return int(''.join(number_views))
+	number_views = scp(text=page_source).xpath('//div[@class="cnt-info_exib"]/b/text()').extract_first()
+	return int(number_views.replace('.',''))
 
 def select_gender(page_source):
 	return scp(text=page_source).xpath('//div[@id="breadcrumb"]/span/a/span/text()').extract()[1]
@@ -48,3 +53,15 @@ def select_music_cipher_url(page_source):
 	if left_links[0] == "#":
 		return left_links[1]
 	return left_links[2]
+
+def select_music_chords(page_source):
+	page_chords = scp(text=page_source).xpath('//div[@id="js-view-script"]/script/text()').extract()[1]
+	try:
+		home = page_chords.find('chords: ') + 8
+		end = page_chords.find('}]);') - 14
+		chords = page_chords[home:end]
+		js = json.loads(chords)
+		return [chord['chord'] for chord in js]
+	except Exception as e:
+		return []
+	
